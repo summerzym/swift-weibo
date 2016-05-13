914a12d45eba0733f5e03de37bcc1e98237aa23d
zhaoym <zhaoym@retechcorp.com>
2016-05-13 11:55:18 +0800

    自定义转场

--- swift-weibo/swift-weibo/Classes/Home/HomeTableViewController.swift
+++ swift-weibo/swift-weibo/Classes/Home/HomeTableViewController.swift
@@ -1,50 +1,85 @@
-
 //
 //  HomeTableViewController.swift
-//  swift-weibo
+//  XMGWB
 //
-//  Created by SummerZhao on 16/5/10.
-//  Copyright © 2016年 yunmei. All rights reserved.
+//  Created by 李南江 on 15/9/6.
+//  Copyright © 2015年 xiaomage. All rights reserved.
 //
 
 import UIKit
 
 class HomeTableViewController: BaseTableViewController {
-
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // 1.如果没有登录，就设置未登录界面信息
+        
         if !userLogin{
+            
             visitorView?.setupVisitorInfo(true, imageName: "visitordiscover_feed_image_house", message: "关注一些人，回这里看看有什么惊喜")
-            return
         }
+        // 初始化导航条按钮
+        setupNavgationItem()
         
-        //2.初始化导航条
-        setUpNav()
     }
     
-    private func setUpNav(){
-        //1.初始化左边 右边按钮
-        navigationItem.leftBarButtonItem = UIBarButtonItem.createBarButtonItem("navigationbar_friendattention", target: self, action: "leftItemClick")
-        navigationItem.rightBarButtonItem = UIBarButtonItem.createBarButtonItem("navigationbar_pop", target: self, action: "rightItemClick")
+    func letBtnClick()
+    {
+        print(__FUNCTION__);
+    }
+    func rightBtnClick()
+    {
+        print(__FUNCTION__);
+    }
+    
+    // MARK: - 内部控制方法
+    /**
+    初始化导航条按钮
+    */
+    private func setupNavgationItem()
+    {
+        // 1.添加左右按钮
+        navigationItem.leftBarButtonItem = UIBarButtonItem.createBarButtonItem("navigationbar_friendattention", target: self, action: "letBtnClick")
+        navigationItem.rightBarButtonItem = UIBarButtonItem.createBarButtonItem("navigationbar_pop", target: self, action: "rightBtnClick")
         
+        // 2.添加标题按钮
         let titleBtn = TitleButton()
-        titleBtn.setTitle("summer " , forState: UIControlState.Normal)
+        titleBtn.setTitle("极客江南 ", forState: UIControlState.Normal)
+        titleBtn.sizeToFit()
         titleBtn.addTarget(self, action: "titleBtnClick:", forControlEvents: UIControlEvents.TouchUpInside)
-
         navigationItem.titleView = titleBtn
+        
     }
     
-    func titleBtnClick(btn: TitleButton){
+    /**
+     监听标题按钮点击
+     
+     :param: btn 标题按钮
+     */
+    func titleBtnClick(btn: TitleButton)
+    {
+        // 1.设置按钮状态
         btn.selected = !btn.selected
+        
+        // 2.创建Storyoard
+        let sb = UIStoryboard(name: "PopoverViewController", bundle: nil)
+        let vc = sb.instantiateInitialViewController()!
+        
+        // 3.设置转场代理, 告诉系统谁来负责转场
+        vc.transitioningDelegate = self
+        // 4.设置转场模式
+        vc.modalPresentationStyle = UIModalPresentationStyle.Custom
+        
+        presentViewController(vc, animated: true, completion: nil)
     }
     
-    func leftItemClick(){
-        print(__FUNCTION__)
-    }
-    
-    func rightItemClick(){
-        print(__FUNCTION__)
+}
+
+extension HomeTableViewController: UIViewControllerTransitioningDelegate
+{
+    // 返回负责转场的控制器对象
+    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController, sourceViewController source: UIViewController) -> UIPresentationController?{
+        
+        return PopoverPresentationController(presentedViewController: presented, presentingViewController: presenting)
     }
 }
