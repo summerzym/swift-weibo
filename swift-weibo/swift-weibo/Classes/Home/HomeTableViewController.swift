52f8a4f354fa3014da03153a360216cfff66b6cc
zhaoym <zhaoym@retechcorp.com>
2016-05-16 11:45:26 +0800

    菜单优化

--- swift-weibo/swift-weibo/Classes/Home/HomeTableViewController.swift
+++ swift-weibo/swift-weibo/Classes/Home/HomeTableViewController.swift
@@ -15,11 +15,14 @@ class HomeTableViewController: BaseTableViewController {
         super.viewDidLoad()
         
         if !userLogin{
-            
             visitorView?.setupVisitorInfo(true, imageName: "visitordiscover_feed_image_house", message: "关注一些人，回这里看看有什么惊喜")
         }
         // 初始化导航条按钮
         setupNavgationItem()
+        
+        // 3.注册通知, 监听菜单
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "change", name: XMGPopoverAnimatorWillShow, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "change", name: XMGPopoverAnimatorWilldismiss, object: nil)
         
     }
     
@@ -31,6 +34,22 @@ class HomeTableViewController: BaseTableViewController {
     {
         print(__FUNCTION__);
     }
+    
+    deinit
+    {
+        // 移除通知
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    /**
+     修改标题按钮的状态
+     */
+    func change(){
+        // 修改标题按钮的状态
+        let titleBtn = navigationItem.titleView as! TitleButton
+        titleBtn.selected = !titleBtn.selected
+    }
+    
     
     // MARK: - 内部控制方法
     /**
@@ -48,38 +67,38 @@ class HomeTableViewController: BaseTableViewController {
         titleBtn.sizeToFit()
         titleBtn.addTarget(self, action: "titleBtnClick:", forControlEvents: UIControlEvents.TouchUpInside)
         navigationItem.titleView = titleBtn
-        
     }
     
     /**
      监听标题按钮点击
-     
      :param: btn 标题按钮
      */
     func titleBtnClick(btn: TitleButton)
     {
-        // 1.设置按钮状态
-        btn.selected = !btn.selected
+        // 1.修改箭头方向
+        //        btn.selected = !btn.selected
         
-        // 2.创建Storyoard
-        let sb = UIStoryboard(name: "PopoverViewController", bundle: nil)
-        let vc = sb.instantiateInitialViewController()!
+        // 2.弹出菜单
+        let sb = UIStoryboard(name: "FriendGroup", bundle: nil)
+        let vc = sb.instantiateInitialViewController()
+        // 2.1设置转场代理
+        // 默认情况下modal会移除以前控制器的view, 替换为当前弹出的view
+        // 如果自定义转场, 那么就不会移除以前控制器的view
+        //        vc?.transitioningDelegate = self
+        vc?.transitioningDelegate = popverAnimator
         
-        // 3.设置转场代理, 告诉系统谁来负责转场
-        vc.transitioningDelegate = self
-        // 4.设置转场模式
-        vc.modalPresentationStyle = UIModalPresentationStyle.Custom
+        // 2.2设置转场的样式
+        vc?.modalPresentationStyle = UIModalPresentationStyle.Custom
         
-        presentViewController(vc, animated: true, completion: nil)
+        presentViewController(vc!, animated: true, completion: nil)
     }
     
+    // MARK: - 懒加载
+    // 一定要定义一个属性来报错自定义转场对象, 否则会报错
+    private lazy var popverAnimator:PopoverAnimator = {
+        let pa = PopoverAnimator()
+        pa.presentFrame = CGRect(x: 100, y: 56, width: 200, height: 350)
+        return pa
+    }()
 }
 
-extension HomeTableViewController: UIViewControllerTransitioningDelegate
-{
-    // 返回负责转场的控制器对象
-    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController, sourceViewController source: UIViewController) -> UIPresentationController?{
-        
-        return PopoverPresentationController(presentedViewController: presented, presentingViewController: presenting)
-    }
-}
--- swift-weibo/swift-weibo/Classes/Home/popover/PopoverPresentationController.swift
+++ swift-weibo/swift-weibo/Classes/Home/popover/PopoverPresentationController.swift
@@ -1,57 +1,63 @@
 //
 //  PopoverPresentationController.swift
-//  swift-weibo
+//  DSWeibo
 //
-//  Created by SummerZhao on 16/5/13.
-//  Copyright © 2016年 yunmei. All rights reserved.
+//  Created by xiaomage on 15/9/9.
+//  Copyright © 2015年 小码哥. All rights reserved.
 //
 
 import UIKit
 
 class PopoverPresentationController: UIPresentationController {
     
+    /// 定义属性保存菜单的大小
+    var presentFrame = CGRectZero
+    
     /**
-     实例化负责转场的控制器
+     初始化方法, 用于创建负责转场动画的对象
      
      :param: presentedViewController  被展现的控制器
-     :param: presentingViewController 发起转场的控制器, xocde6是nil, xocde7是野指针
+     :param: presentingViewController 发起的控制器, Xocde6是nil, Xcode7是野指针
      
-     :returns: 负责转场的控制器
+     :returns: 负责转场动画的对象
      */
     override init(presentedViewController: UIViewController, presentingViewController: UIViewController) {
         super.init(presentedViewController: presentedViewController, presentingViewController: presentingViewController)
     }
     
-    /*
-    即将布局容器视图上得子视图时调用
-    containerView 容器视图, 放置展现出来的视图
-    presentedView 被展现的视图
-    */
-    override func containerViewWillLayoutSubviews(){
-        // 1.添加遮盖
-        //        containerView?.addSubview(coverView)
-        containerView?.insertSubview(coverView, atIndex: 0)
-        coverView.frame = (containerView?.frame)!
-        
-        // 2.调整展现视图大小
-        presentedView()?.frame = CGRect(x: 100, y: 56, width: 200, height: 200)
-    }
-    
     /**
-     关闭弹窗
+     即将布局转场子视图时调用
      */
-    func close(){
-        presentedViewController.dismissViewControllerAnimated(true, completion: nil)
+    override func containerViewWillLayoutSubviews()
+    {
+        // 1.修改弹出视图的大小
+        if presentFrame == CGRectZero{
+            
+            presentedView()?.frame = CGRect(x: 100, y: 56, width: 200, height: 200)
+        }else
+        {
+            presentedView()?.frame = presentFrame
+        }
+        
+        // 2.在容器视图上添加一个蒙版, 插入到展现视图的下面
+        containerView?.insertSubview(coverView, atIndex: 0)
     }
     
     // MARK: - 懒加载
-    lazy var coverView: UIView = {
-        // 1.创建蒙版
+    private lazy var coverView: UIView = {
+        // 1.创建view
         let view = UIView()
         view.backgroundColor = UIColor(white: 0.0, alpha: 0.2)
-        // 2.注册监听
+        view.frame = UIScreen.mainScreen().bounds
+        
+        // 2.添加监听
         let tap = UITapGestureRecognizer(target: self, action: "close")
         view.addGestureRecognizer(tap)
         return view
     }()
+    
+    func close(){
+        // presentedViewController拿到当前弹出的控制器
+        presentedViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
